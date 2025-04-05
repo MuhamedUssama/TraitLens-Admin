68473d4954ab76c1617f706b6fa4d6b28e83494c
Mohamed Osama <mohamed.osama200538@gmail.com>
2025-04-05 19:50:41 +0200

    Created users tab view model and it's states

--- lib/core/di/di.config.dart
+++ lib/core/di/di.config.dart
@@ -36,6 +36,8 @@ import '../../features/tabs/users/data/repositories/users_repository.dart'
     as _i621;
 import '../../features/tabs/users/data/repositories_impl/users_repository_impl.dart'
     as _i650;
+import '../../features/tabs/users/view/view_models/users_tab_view_model/users_tab_view_model.dart'
+    as _i947;
 import '../cache/shared_preferences.dart' as _i254;
 
 extension GetItInjectableX on _i174.GetIt {
@@ -52,6 +54,9 @@ extension GetItInjectableX on _i174.GetIt {
     gh.singleton<_i364.AuthDataSource>(() => _i105.AuthDataSourceImpl());
     gh.factory<_i621.UsersRepository>(
       () => _i650.UsersRepositoryImpl(gh<_i685.UsersRemoteDataSource>()),
+    );
+    gh.factory<_i947.UsersTabViewModel>(
+      () => _i947.UsersTabViewModel(gh<_i621.UsersRepository>()),
     );
     gh.factory<_i961.AuthRepository>(
       () => _i954.AuthRepositoryImpl(gh<_i364.AuthDataSource>()),
---
+++ lib/features/tabs/users/view/view_models/users_tab_view_model/users_tab_view_model.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
+import 'package:trait_lens_admin/features/tabs/users/data/repositories/users_repository.dart';
+
+import 'users_tab_states.dart';
+
+@injectable
+class UsersTabViewModel extends Cubit<UsersTabStates> {
+  final UsersRepository _usersRepository;
+
+  @factoryMethod
+  UsersTabViewModel(this._usersRepository) : super(UsersTabInitialState());
+
+  Future<void> getAllUsers() async {
+    emit(UsersTabLoadingState());
+
+    final result = await _usersRepository.getUsers();
+
+    result.fold(
+      (error) => emit(UsersTabErrorState(error.toString())),
+      (usersList) => emit(UsersTabLoadedState(usersList)),
+    );
+  }
+}

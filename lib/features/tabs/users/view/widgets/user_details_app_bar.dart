8340fd3d6908a1e8fde4951c98e4aef69670c1db
Mohamed Osama <mohamed.osama200538@gmail.com>
2025-04-06 10:07:08 +0200

    Created loading widget using shimmer effect for image background in details screen

--- lib/features/tabs/users/view/widgets/user_details_app_bar.dart
+++ lib/features/tabs/users/view/widgets/user_details_app_bar.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:trait_lens_admin/core/constants/app_constants.dart';
 import 'package:trait_lens_admin/core/theme/app_theme.dart';
+import 'package:trait_lens_admin/features/tabs/users/view/widgets/background_image_loading_widget.dart';
 
 class UserDetailsAppBar extends StatelessWidget {
   final String? imageUrl;
@@ -20,6 +21,8 @@ class UserDetailsAppBar extends StatelessWidget {
       flexibleSpace: FlexibleSpaceBar(
         background: CachedNetworkImage(
           imageUrl: imageUrl ?? AppConstants.noFoundImage,
+          placeholder: (context, url) => BackgroundImageLoadingWidget(),
+          errorWidget: (context, url, error) => BackgroundImageLoadingWidget(),
           fit: BoxFit.cover,
         ),
       ),

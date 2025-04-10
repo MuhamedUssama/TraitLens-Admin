d809f62070f7d9bf27ebea79cd3653b99a8f4120
Mohamed Osama <mohamed.osama200538@gmail.com>
2025-04-10 14:30:35 +0200

    Created notification tab states

---
+++ lib/features/tabs/notifications/view/view_models/notifications_tab_view_model/notifications_tab_states.dart
@@ -0,0 +1,19 @@
+import 'package:trait_lens_admin/features/tabs/notifications/data/models/notification_model.dart';
+
+abstract class NotificationsTabStates {}
+
+class NotificationsTabInitialState extends NotificationsTabStates {}
+
+class NotificationsTabLoadingState extends NotificationsTabStates {}
+
+class NotificationsTabLoadedState extends NotificationsTabStates {
+  final List<NotificationModel> notifications;
+
+  NotificationsTabLoadedState(this.notifications);
+}
+
+class NotificationsTabErrorState extends NotificationsTabStates {
+  final String error;
+
+  NotificationsTabErrorState(this.error);
+}

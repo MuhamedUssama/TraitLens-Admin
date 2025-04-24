0f29edc833ca2eceee8e747d85436bb80ecbed20
Mohamed Osama <mohamed.osama200538@gmail.com>
2025-04-24 17:57:12 +0200

    Created repository module

--- lib/features/tabs/users/data/repositories/users_repository.dart
+++ lib/features/tabs/users/data/repositories/users_repository.dart
@@ -1,7 +1,10 @@
 import 'package:dartz/dartz.dart';
 import 'package:trait_lens_admin/core/errors/exceptions.dart';
 import 'package:trait_lens_admin/core/models/user_details_model.dart';
+import 'package:trait_lens_admin/features/tabs/users/data/models/detection_result_model.dart';
 
 abstract interface class UsersRepository {
   Future<Either<ServerException, List<UserDetailsModel>>> getUsers();
+
+  Future<Either<ServerException, List<DetectionResultModel>>> getUserResults();
 }
--- lib/features/tabs/users/data/repositories_impl/users_repository_impl.dart
+++ lib/features/tabs/users/data/repositories_impl/users_repository_impl.dart
@@ -4,6 +4,7 @@ import 'package:trait_lens_admin/core/errors/exceptions.dart';
 import 'package:trait_lens_admin/core/helpers/connectivity_helper.dart';
 import 'package:trait_lens_admin/core/models/user_details_model.dart';
 import 'package:trait_lens_admin/features/tabs/users/data/data_sources/users_remote_data_source.dart';
+import 'package:trait_lens_admin/features/tabs/users/data/models/detection_result_model.dart';
 import 'package:trait_lens_admin/features/tabs/users/data/repositories/users_repository.dart';
 
 @Injectable(as: UsersRepository)
@@ -17,6 +18,16 @@ class UsersRepositoryImpl implements UsersRepository {
   Future<Either<ServerException, List<UserDetailsModel>>> getUsers() async {
     if (await ConnectivityHelper.checkInternetConnection()) {
       return await _usersRemoteDataSource.getAllUserProfilesForAllUsers();
+    } else {
+      return Left(NoInternetConnectionException());
+    }
+  }
+
+  @override
+  Future<Either<ServerException, List<DetectionResultModel>>>
+  getUserResults() async {
+    if (await ConnectivityHelper.checkInternetConnection()) {
+      return _usersRemoteDataSource.getUserResults();
     } else {
       return Left(NoInternetConnectionException());
     }
